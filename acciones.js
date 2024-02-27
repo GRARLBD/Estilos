675db7300d83a71c271bc570c61e4544b7e55a71
grarlbd <grarlbdmirage@gmail.com>
2024-02-27 00:20:42 -0300

    fdfd

--- acciones.js
+++ acciones.js
@@ -35,22 +35,75 @@ var gateway = `ws://${window.location.hostname}/ws`;
   function toggle(){
     //websocket.send('toggle');
     var selectedCirculo = 6;
-      var caja = document.getElementById("caja");
-      var x = "39%"; 
-      var y = "22%"; 
-      var espaciado = "6%"; 
-      var salto = 0;
-      var limite = 1;
-      var index = 0;
+    var caja = document.getElementById("caja");
 
-      for (var i = 0; i < limite; i++, salto++) {
-        index += 1;
-        var circulo = document.createElement("div");
-        circulo.classList.add("circulo");
-        circulo.id = "circulo" + (i + 1);
+    var x = "39%"; 
+    var y = "22%"; 
+    var espaciado = "6%"; 
+    var salto = 0;
+    var limite = 4;
+    var index = 0;
 
-        caja.appendChild(circulo);
+    for (var i = 0; i < limite; i++, salto++) {
+      index += 1;
+      var circulo = document.createElement("div");
+      circulo.classList.add("circulo");
+      circulo.id = "circulo" + (i + 1);
+      var posicionX = "calc(" + x + " + (" + salto + " * " + espaciado + "))";
+      circulo.style.top = y;
+      circulo.style.left = posicionX;
+      caja.appendChild(circulo);
 
+      if (index === selectedCirculo) {
+        selectedCirculo = circulo;
+        selectedCirculo.style.backgroundColor = "green"; 
+      }
+
+    }
+
+    x = "48%";
+    y = "31%"; 
+    espaciado = "6%";
+    var salto = 0;
+    var limite = 1;
+
+    for (var i = 0; i < limite; i++, salto++) {
+      index += 1;
+      var circulo = document.createElement("div");
+      circulo.classList.add("circulo");
+      circulo.id = "circulo" + (index);
+      var posicionX = "calc(" + x + " + (" + salto + " * " + espaciado + "))";
+      circulo.style.top = y;
+      circulo.style.left = posicionX;
+
+      caja.appendChild(circulo);
+      if (index === selectedCirculo) {
+        selectedCirculo = circulo;
+        selectedCirculo.style.backgroundColor = "green"; // Cambiar el color del círculo resaltado a verde
+      }
+    }
+
+
+    x = "48%"; 
+    y = "41%"; 
+    espaciado = "6%"; 
+    var salto = 0;
+    var limite = 1;
+
+    for (var i = 0; i < limite; i++, salto++) {
+      index += 1;
+      var circulo = document.createElement("div");
+      circulo.classList.add("circulo");
+      circulo.id = "circulo" + (i + 1);
+      var posicionX = "calc(" + x + " + (" + salto + " * " + espaciado + "))";
+
+      circulo.style.top = y;
+      circulo.style.left = posicionX;
+      caja.appendChild(circulo);
+      if (index === selectedCirculo) {
+        selectedCirculo = circulo;
+        selectedCirculo.style.backgroundColor = "green"; // Cambiar el color del círculo resaltado a verde
+      }
+    }
       
-      }  
   }
